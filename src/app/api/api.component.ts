68a3e9641001df0d41621249351aec1f8c4182e3
thiago_osim <thiago_osim@hotmail.com>
2024-06-13 23:36:16 -0300

    Adicionado captura de dados da api externa

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { Imagem } from '../models/api';
 
 @Component({
   selector: 'app-api',
@@ -10,11 +11,22 @@ import { ApiService } from '../services/api.service';
 })
 export class ApiComponent {
 
+  imagens: any;
+  erro: any;
   constructor(private apiService: ApiService) {
     this.getImg();
   }
 
   getImg() {
-    this.apiService.getImg().subscribe();
+    this.apiService.getImg().subscribe(
+      (data: Imagem) => {
+        this.imagens = data;
+        console.log('Dados sendo recebidos ', data);
+      },
+      (error: any) => {
+        this.erro = error;
+        console.log('Erro ', error)
+      }
+    );
   }
 }
